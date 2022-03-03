5f247a7d4df6aaf38e01296bd621fddc114088b1
Kamden Rasmussen <kamdenr@gmail.com>
2022-03-03 16:18:23 -0700

    refactor

--- SchoolProjects/se4200/canvasAssignmentTracker/Server/model.js
+++ SchoolProjects/se4200/canvasAssignmentTracker/Server/model.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-    "mongodb+srv://web4200:stim3lep9CRAG.firn@cluster0.fgaom.mongodb.net/cars?retryWrites=true&w=majority"
+    ""
 );
 
 const assignment = mongoose.model("Assignment", {
